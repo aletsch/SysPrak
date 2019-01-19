61bf16fbaeb3004904188c61a2512a1bde74d0fa
Julius Blumenroeder <julius.b96@web.de>
2019-01-19 16:14:44 +0100

    erste Versuche mögliche Züge zu finden

--- think.c
+++ think.c
@@ -20,7 +20,7 @@ int think() {
       for(int x=0, x<8, x++) {
         for (int y=7, y>=0, y--) {
           if (spieldaten.field[x][y] == ('w' || 'W')) {
-            if (possibleMovesWhite() == 1) {
+            if (possibleMovesWhite(x,y) == 1) {
               return 0;
             }
           }
@@ -37,12 +37,70 @@ int think() {
   }
 }
 
+
+
+
+
 //hier werden alle züge einer weißen Figur auf Gültigkeit geprüft
-int possibleMovesWhite() {
-  if (moveFound) {
-    //TODO move in die pipe schreiben
-    return 1;
+
+//falls 'out of bounds'- artige Fehlermeldungen kommen -> if-statements jeweils um
+//"&& (x+1<8) && (x+1>-1) && (y+1<8) && (y+1>-1)" oder Ähnliches ergänzen
+int possibleMovesWhite(int x, int y) {
+  //pawn move
+  if (spieldaten.field[x][y] == 'w') {
+    //nach links oben
+    if (spieldaten.field[x-1][y+1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    } else if ((spieldaten.field[x-1][y+1] == ('b' || 'B')) && (spieldaten.field[x-2][y+2] == '*')){
+      //TODO diesen move in "schlagende Züge"-Datei schreiben
+      //bzw eigentlich nach weiteren, den Zug vollendenden, schlagenden Zügen suchen (rekursiv?)
+    }
+    //nach rechts open
+    if (spieldaten.field[x+1][y+1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    } else if ((spieldaten.field[x+1][y+1] == ('b' || 'B')) && (spieldaten.field[x+2][y+2] == '*')){
+      //TODO diesen move in "schlagende Züge"-Datei schreiben
+      //bzw eigentlich nach weiteren, den Zug vollendenden, schlagenden Zügen suchen (rekursiv?)
+    }
+
   }
+  //Queen move
+  if (spieldaten.field[x][y] == 'W') {
+
+    //TODO erstmal nachschauen ob schon ein schlagender Zug gefunden wurde
+
+    //TODO schauen ob Queen schlagen kann
+
+
+
+
+    //nach nicht schlagenden Zügen suchen
+      //hier fehlt noch: Dame kann theoretisch weiter als nur ein Feld laufen
+
+    //nach links oben
+    if (spieldaten.field[x-1][y+1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    }
+    //nach rechts oben
+    if (spieldaten.field[x+1][y+1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    }
+    //nach links unten
+    if (spieldaten.field[x-1][y-1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    }
+    //nach rechts unten
+    if (spieldaten.field[x+1][y-1] == '*') {
+      //TODO diesen move in "nicht schlagende Züge"-Datei schreiben
+    }
+  }
+
+
+// ünnötig geworden durch 2-Dateien Idee
+  // if (moveFound) {
+  //   //TODO move in die pipe schreiben
+  //   return 1;
+  // }
   return 0;
 }
 
